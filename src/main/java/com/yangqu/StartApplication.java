a25dfb2c633f6ffa82f32078b35e073813787e10
shitao.yang <total_young@163.com>
2018-06-01 17:29:08 +0800

    2018年6月1日17:28:59

---
+++ src/main/java/com/yangqu/StartApplication.java
@@ -0,0 +1,17 @@
+package com.yangqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yangst on 2018/6/1.
+ */
+
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+
+    }
+}
